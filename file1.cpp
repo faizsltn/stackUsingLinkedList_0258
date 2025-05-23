bb9e4fbedb85b35ae477d5866c9cda624cf5b451
(faizsltn) <(faizsulthondaudm@gmail.com)>
2025-05-23 07:54:25 +0700

    membuat fungsi

--- file1.cpp
+++ file1.cpp
@@ -26,5 +26,16 @@ public:
         top = NULL; //initialize the stuck with a null top pointer
     }
 
-    //
+    //push operation: insert an element onto the top of the stack 
+    int push(int value)
+    {
+        Node *newNode = new Node(); // 1. allocate memory for the new node
+        newNode->data = value;      // 2. assing value
+        newNode->next = top;        // 3. set the next pointer of the new node to....
+        top = newNode;              // 4. update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    
 };
